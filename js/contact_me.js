6f1856a589ad076ef224d7efdf9e00731b0865e6
Eric Dooley <ericdooley@gmail.com>
2015-02-23 09:48:18 -0600

    Fixed bug skipping client side validations in contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -5,30 +5,28 @@ $(function() {
         submitError: function($form, event, errors) {
             // additional error messages or events
         },
+        submitSuccess: function($form, e) {
+          e.preventDefault();
+          $form.hide();
+          $('#contactFormSuccess').html('<div class="alert alert-info"><i class="fa fa-cog fa-spin"></i>Sending your inquiry...</div>');
+          setTimeout(function() { 
+            $.ajax({
+              type: "POST",
+              url: "https://formkeep.com/f/83fe71fb1ec5",
+              data: $form.serialize(),
+              success: function() {
+                  $('#contactFormSuccess').html("<div class='alert alert-success'><i class='fa fa-smile-o'></i>Thanks! We'll be in touch with you shortly.");
+              },
+              error: function() {
+                  $('#contactFormSuccess').html("<div class='alert alert-danger'><i class='fa fa-frown-o'></i>Something went wrong. Please try again later!</div>");
+                  $form.show();
+              }
+            })
+          }, 1200);
+        },
         filter: function() {
             return $(this).is(":visible");
         }
-    });
-    
-    $('#contactForm').on('submit', function(e) {
-        e.preventDefault();
-        var $form = $(this);
-        $form.hide();
-        $('#contactFormSuccess').html('<div class="alert alert-info"><i class="fa fa-cog fa-spin"></i>Sending your inquiry...</div>');
-        setTimeout(function() { 
-          $.ajax({
-            type: "POST",
-            url: "https://formkeep.com/f/83fe71fb1ec5",
-            data: $form.serialize(),
-            success: function() {
-                $('#contactFormSuccess').html("<div class='alert alert-success'><i class='fa fa-smile-o'></i>Thanks! We'll be in touch with you shortly.");
-            },
-            error: function() {
-                $('#contactFormSuccess').html("<div class='alert alert-danger'><i class='fa fa-frown-o'></i>Something went wrong. Please try again later!</div>");
-                $form.show();
-            }
-          })
-        }, 1200);
     });
 
     $("a[data-toggle=\"tab\"]").click(function(e) {
